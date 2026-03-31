6d78921213a39a834e06090b8bcdfd2ba1d2b72c
Mohamed <mido.basiouny123@gmail.com>
2026-03-31 06:23:13 +0200

    creating : customLayoutBuilder Widget

---
+++ lib/views/custom_layout_builder.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomLayoutBuilder extends StatelessWidget {
+  const CustomLayoutBuilder({
+    super.key,
+    required this.mobileLayout,
+    required this.tabletLayout,
+    required this.desktopLayout,
+  });
+
+  final WidgetBuilder mobileLayout, tabletLayout, desktopLayout;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constrains) {
+        if (constrains.maxWidth < 600) {
+          return mobileLayout(context);
+        } else if (constrains.maxWidth < 900) {
+          return tabletLayout(context);
+        } else {
+          return desktopLayout(context);
+        }
+      },
+    );
+  }
+}
