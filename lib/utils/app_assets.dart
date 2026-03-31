cf855b0fa1b3488f3f2983d74f9320ffae477f26
Mohamed <mido.basiouny123@gmail.com>
2026-03-31 08:40:55 +0200

    adding assets to the project & creating AppAssets class

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  static const String imagesAvatar1 = "assets/images/avatar_1.svg";
+
+  static const String imagesAvatar2 = "assets/images/avatar_2.svg";
+
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  static const String imagesCard = "assets/images/card.svg";
+
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  static const String imagesIncome = "assets/images/income.svg";
+
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  static const String imagesMyInvestments = "assets/images/my_investments.svg";
+
+  static const String imagesMyTransactions = "assets/images/my_transactions.svg";
+
+  static const String imagesSettings = "assets/images/settings.svg";
+
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  static const String imagesWalletAccount = "assets/images/wallet_account.svg";
+}
+
