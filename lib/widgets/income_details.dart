9c696ea83de6a11995e999c475bacbbaa5c6db58
Mohamed <mido.basiouny123@gmail.com>
2026-04-13 00:59:11 +0200

    Remove unwanted imports

--- lib/widgets/income_details.dart
+++ lib/widgets/income_details.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
-import 'package:test1/models/income_item_model.dart';
 import 'package:test1/utils/app_assets.dart';
-import 'package:test1/widgets/income_details_item.dart';
 import 'package:test1/widgets/income_items_list_view.dart';
 
 class IncomeDetails extends StatelessWidget {
