bae21457e0bf8714a179bc6d1b5d352a8ad66f0a
Mohamed <mido.basiouny123@gmail.com>
2026-04-12 21:33:51 +0200

    Create TransactionItemModel

---
+++ lib/models/transaction_item_model.dart
@@ -0,0 +1,9 @@
+class TransactionItemModel {
+  final String title, subTitle, trailing;
+
+  TransactionItemModel({
+    required this.title,
+    required this.subTitle,
+    required this.trailing,
+  });
+}
