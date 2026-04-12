b58856a4569f91b4239899f54ee9043c7cfe1ae6
Mohamed <mido.basiouny123@gmail.com>
2026-04-13 00:52:20 +0200

    Create IncomeItemsListView widget to display a list of income items

---
+++ lib/widgets/income_items_list_view.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:test1/models/income_item_model.dart';
+import 'package:test1/widgets/income_details_item.dart';
+
+class IncomeItemsListView extends StatelessWidget {
+  const IncomeItemsListView({super.key});
+
+  static const List<IncomeItemModel> modelList = [
+    IncomeItemModel(
+      circleColor: Color(0xff208CC8),
+      text: 'Design service',
+      percentage: '40',
+    ),
+    IncomeItemModel(
+      circleColor: Color(0xff4EB7F2),
+      text: 'Design product',
+      percentage: '25',
+    ),
+    IncomeItemModel(
+      circleColor: Color(0xff064061),
+      text: 'Product royalti',
+      percentage: '20',
+    ),
+    IncomeItemModel(
+      circleColor: Color(0xffE2DECD),
+      text: 'Other',
+      percentage: '22',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: modelList.map((e) => IncomeDetailsItem(model: e)).toList(),
+    );
+  }
+}
