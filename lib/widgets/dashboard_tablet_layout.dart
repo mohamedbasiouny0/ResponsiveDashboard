5f6f3ee0b42f7b88501b48a068e6074c0f305f2c
Mohamed <mido.basiouny123@gmail.com>
2026-04-13 05:05:20 +0200

    Refactor: DashboardTabletLayout

--- lib/widgets/dashboard_tablet_layout.dart
+++ lib/widgets/dashboard_tablet_layout.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:test1/widgets/all_expenses.dart';
 import 'package:test1/widgets/custom_drawer.dart';
-import 'package:test1/widgets/mid_section.dart';
+import 'package:test1/widgets/income_section.dart';
+import 'package:test1/widgets/my_card_section.dart';
+import 'package:test1/widgets/quick_invoice.dart';
+import 'package:test1/widgets/transaction_history_section.dart';
 
 class DashboardTabletLayout extends StatelessWidget {
   const DashboardTabletLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    print(MediaQuery.widthOf(context));
-    return const Row(
+    return Row(
       children: [
-        Expanded(child: CustomDrawer()),
-        Expanded(flex: 3, child: MidSection()),
+        const CustomDrawer(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(32),
+            child: ListView(
+              children: const [
+                AllExpenses(),
+                Gap(24),
+                QuickInvoice(),
+                Gap(24),
+                MyCardSection(),
+                Gap(24),
+                TransactionHistorySection(),
+                Gap(24),
+                IncomeSection(),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
